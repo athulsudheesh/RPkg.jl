0aeab925502cc476d6d2fc2704687f07dcb1b375
Athul Sudheesh <athuls0708@gmail.com>
2023-01-06 13:21:06 -0600

    CRAN mirror issue resolved
    Defaults to Microsoft CRAN mirror

--- src/RPkg.jl
+++ src/RPkg.jl
@@ -1,6 +1,9 @@
 module RPkg
     using Reexport
     @reexport using RCall
+    R"""
+    options(repos=c(CRAN="https://cran.microsoft.com/"))
+    """
     include("init.jl")
     include("pkgutils.jl")
 end
